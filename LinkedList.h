609d15fc59a288a2b0c841b3341f9f773f87fd6c
BoBo-Ahmed <elbraatube44@gmail.com>
2025-12-08 01:51:06 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -80,7 +80,98 @@ public:
         cout << "\n-----------------------------------------------\n"
              << endl;
     }
+int countTasks()
+    {
+        int count = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+void deleteTask(int id)
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty, nothing to delete." << endl;
+            return;
+        }
 
+        
+        if (head->id == id)
+        {
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+            cout << "Task with ID " << id << " has been deleted." << endl;
+            return;
+        }
+
+        Node *temp = head;
+        Node *prev = NULL;
+        bool found = false;
+
+        while (temp != NULL)
+        {
+            if (temp->id == id)
+            {
+                prev->next = temp->next;
+                delete temp;
+                found = true;
+                cout << "Task with ID " << id << " has been deleted." << endl;
+                return;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (!found)
+        {
+            cout << "Task with ID " << id << " not found." << endl;
+        }
+    }
+void deleteAllTasks()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            Node *nextNode = temp->next;
+            delete temp;
+            temp = nextNode;
+        }
+        head = NULL;
+        cout << "All tasks have been deleted." << endl;
+    }
+void markAsDone(int id)
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty, nothing to mark as done." << endl;
+            return;
+        }
+
+        Node *temp = head;
+        bool found = false;
+
+        while (temp != NULL)
+        {
+            if (temp->id == id)
+            {
+                temp->status = "done";
+                found = true;
+                cout << "Task ID " << id << " has been marked as done." << endl;
+                return;
+            }
+            temp = temp->next;
+        }
+
+        if (!found)
+        {
+            cout << "Task ID " << id << " not found." << endl;
+        }
+    }
     // Masry
     void searchTaskID(int id)
     {
